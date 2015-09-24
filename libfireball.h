e92e8ee0da085598eb9243a807c4caec65b5e694
Bailey Seymour <bailey@baileyseymour.com>
2015-09-24 16:40:36 -0400

    comments ^-^

--- libfireball.h
+++ libfireball.h
@@ -3,7 +3,13 @@
 #import <spawn.h>
 #import <Social/Social.h>
 
+// Call after [super viewWillAppear:animated]
 void FBSetPrefsTint(UIViewController *prefs, UIColor *tintColor);
+
+// Call after [super viewWillDisappear:animated]
 void FBResetPrefsTint(UIViewController *prefs);
+
 void FBKillProcess(NSString *signal, NSString *processName);
+
+// Call this in -loadView to add a share button.
 void FBAddTwitterShareButton(UIViewController *prefs, UIImage *twitterIcon, NSString *shareText);
